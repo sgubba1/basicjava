e971262ec20d39a854e8f0275d05ec9b4afb56aa
Srinidhi <gubbasrini@gmail.com>
2017-08-01 14:13:43 -0700

    My fourth notes

---
+++ looplesson/src/looplesson/Application.java
@@ -0,0 +1,14 @@
+package looplesson;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int  x;
+		for(x=0 ; x<10; x=x+1) {
+			System.out.println("Hello World");
+		}
+	}
+
+}
